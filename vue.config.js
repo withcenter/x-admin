bf1bfb8f84777fe6b3ab4c4cbfca43502a676f05
JaeHo Song <thruthesky@gmail.com>
2021-06-29 21:00:07 +0900

    update on admin

--- vue.config.js
+++ vue.config.js
@@ -1,39 +1,16 @@
 const fs = require("fs");
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/view/admin/" : "/",
-  // outputDir: "/Users/thruthesky/www/docker/home/centerx/view/x",
-  outputDir: "../../docker/home/centerx/view/admin",
-
-  // pluginOptions: {
-  //   webpackBundleAnalyzer: {
-  //     openAnalyzer: false,
-  //   },
-  // },
-
-  // chainWebpack: (config) => {
-  //   config.performance.maxEntrypointSize(800000).maxAssetSize(600000);
-  // },
-  // pwa: {
-  //   name: "ClientX",
-  //   themeColor: "#ffffff",
-  //   msTileColor: "#000000",
-  //   assetsVersion: 'abcd1234', //Date.now(),
-  //   appleMobileWebAppCapable: "yes",
-  //   appleMobileWebAppStatusBarStyle: "black",
-  //   workboxOptions: {
-  //     exclude: [/\.php$/],
-  //   },
-  // },
-
-  // devServer: {
-  //   disableHostCheck: true,
-  //   host: "main.philov.com",
-  //   port: 4430,
-  //   http2: true,
-  //   https: {
-  //     key: fs.readFileSync("./tmp/ssl/philov.com/private.key"),
-  //     cert: fs.readFileSync("./tmp/ssl/philov.com/cert-ca-bundle.crt"),
-  //   },
-  // },
+  publicPath: (function () {
+    if (process.env.NODE_ENV === "production") return "/view/admin/";
+    else if (process.env.NODE_ENV == "flutterkorea") return "/";
+    else return "/";
+  })(),
+  outputDir: (function () {
+    if (process.env.NODE_ENV === "production") {
+      return "../../docker/home/centerx/view/admin";
+    } else if (process.env.NODE_ENV == "flutterkorea") {
+      return "../../docker/home/centerx/view/flutterkorea";
+    } else return "/dist";
+  })(),
 };
